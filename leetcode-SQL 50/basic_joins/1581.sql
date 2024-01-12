c72a5f61735b291b7b7a82a63bbdd4938cdca6ef
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-12 13:44:29 +0900

    1581-Customer Who Visited but Did Not Make Any Transactions

---
+++ leetcode-SQL 50/basic_joins/1581.sql
@@ -0,0 +1,9 @@
+-- 1581 Customer Who Visited but Did Not Make Any Transactions
+SELECT
+    v.customer_id, count(v.visit_id) AS "count_no_trans"
+FROM 
+    Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
