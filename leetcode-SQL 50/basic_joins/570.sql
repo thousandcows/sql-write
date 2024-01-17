3baf021814738a98ac0de05bba4ae87daaacecaa
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-17 13:08:26 +0900

    570-Managers with at Least 5 Direct Reports

--- leetcode-SQL 50/basic_joins/570.sql
+++ leetcode-SQL 50/basic_joins/570.sql
@@ -1,1 +1,13 @@
 -- Managers with at Least 5 Direct Reports
+SELECT
+    e.name
+FROM
+    Employee e
+JOIN 
+    Employee ee
+ON 
+    e.id = ee.managerId
+GROUP BY 
+    e.id, e.name
+HAVING 
+    count(*) >= 5;
