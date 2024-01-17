fea550e3158bc18d3ed9e745d503615b6564d318
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-17 13:35:18 +0900

    -- 1934.Confirmation Rate with aggregate functions

---
+++ leetcode-SQL 50/basic_joins/1934.sql
@@ -0,0 +1,16 @@
+-- 1934.Confirmation Rate
+SELECT
+    s.user_id,
+    COALESCE(
+        ROUND(
+            SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(s.user_id)::numeric, 2)
+        , 0.00)
+        AS "confirmation_rate"
+FROM
+    Signups s
+LEFT JOIN
+    Confirmations c
+ON 
+    s.user_id = c.user_id 
+GROUP BY 
+    s.user_id;
