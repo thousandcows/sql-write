7bbdb31f8bf13575f83fbcef26e8a1a8654061e5
HYUNWOO CHEON <milcamboo@gmail.com>
2024-01-25 09:35:01 +0900

    Monthly Transactions 1: with case, to_char, and aggregate func

---
+++ leetcode-SQL 50/basic_aggregate_functions/1193.sql
@@ -0,0 +1,14 @@
+-- 1193. Monthly Transactions 1
+SELECT
+    TO_CHAR(t.trans_date, 'YYYY-MM') AS month,
+    t.country,
+    COUNT(*) AS trans_count,
+    SUM(CASE WHEN t.state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(t.amount) AS trans_total_amount,
+    SUM(CASE WHEN t.state = 'approved' THEN t.amount ELSE 0 END) AS approved_total_amount
+FROM 
+    Transactions t
+GROUP BY
+    month, t.country
+ORDER BY
+    month, t.country;
