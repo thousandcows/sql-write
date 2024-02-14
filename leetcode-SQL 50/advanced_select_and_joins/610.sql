35b8b94955419c2f040d652592a9dd73129f7e93
HYUNWOO CHEON <milcamboo@gmail.com>
2024-02-14 09:09:02 +0900

    solve the problem with case and triangle inequality

---
+++ leetcode-SQL 50/advanced_select_and_joins/610.sql
@@ -0,0 +1,11 @@
+-- 610. Triangle Judgement
+SELECT 
+    x,
+    y,
+    z,
+    CASE
+        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM 
+    Triangle;
