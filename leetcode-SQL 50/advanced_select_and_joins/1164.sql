c71dfb083dde400c3f367f587c0203f748dcb376
HYUNWOO CHEON <milcamboo@gmail.com>
2024-02-19 20:30:42 +0900

    solved with coalesce and subquery

---
+++ leetcode-SQL 50/advanced_select_and_joins/1164.sql
@@ -0,0 +1,12 @@
+--1164. Product Price at a Given Date
+SELECT
+    p.product_id,
+    COALESCE(
+        (SELECT new_price
+        FROM Products
+        WHERE product_id = p.product_id AND change_date <= '2019-08-16'
+        ORDER BY change_date DESC
+        LIMIT 1),
+        10) AS price
+FROM
+    (SELECT DISTINCT product_id FROM Products) p;
