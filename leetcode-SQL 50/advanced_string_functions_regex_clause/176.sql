b13cfbdd4eb48be0089a70b6bfccbeea684d263a
HYUNWOO CHEON <milcamboo@gmail.com>
2024-03-13 15:29:19 +0900

    merge branch main

---
+++ leetcode-SQL 50/advanced_string_functions_regex_clause/176.sql
@@ -0,0 +1,7 @@
+-- 176. Second Highest Salary
+SELECT 
+    MAX(salary) AS SecondHighestSalary
+FROM 
+    Employee
+WHERE 
+    salary < (SELECT MAX(salary) FROM Employee);
---
+++ leetcode-SQL 50/basic_joins/175.sql
@@ -0,0 +1,11 @@
+-- 175. Combine Two Tables
+SELECT
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM
+    Person p
+LEFT OUTER JOIN
+    Address a
+ON p.personId = a.personId;
